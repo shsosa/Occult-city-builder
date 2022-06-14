3509a125fa38103a5a6fd0c4cacf8eefd98e057a
shsosa <sh.sosa@gmail.com>
2022-06-14 17:42:20 +0300

    UI works
    the resources scriptable not connected yet

---
+++ Occult city builder/Assets/Scripts/UI/UIMouseHover.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UIMouseHover : MonoBehaviour
+{
+    [SerializeField] private bool isCursorOverObject;
+    private GameObject currentObject;
+    private bool canIstantiateNewBuilding = true;
+    
+    
+   
+    void Update()
+    {
+        CheckIfMouseOverObject();
+        CheckIfCanTakeBuilding();
+        if(isCursorOverObject)
+            GetUIObject();
+        
+    }
+
+    private void CheckIfMouseOverObject()
+    {
+        if (GetMousePos())
+        {
+        
+            isCursorOverObject = true;
+            
+        }
+        else
+        {
+            isCursorOverObject = false;
+        }
+    }
+
+    bool GetMousePos()
+    {
+        
+        return  EventSystem.current.IsPointerOverGameObject();
+    }
+
+    void CheckIfCanTakeBuilding()
+    {
+        
+       
+        if (isCursorOverObject && Input.GetMouseButtonDown(0))
+        {
+            currentObject = GetUIObject();
+            currentObject.GetComponent<UIObject>().CreateBuldingFromUI();
+            canIstantiateNewBuilding = false;
+            
+        }
+        else
+        {
+            canIstantiateNewBuilding = true;
+        }
+    }
+
+    
+
+    GameObject GetUIObject()
+    {
+        var eventData = new PointerEventData(EventSystem.current);
+        eventData.position = Input.mousePosition;
+        var results = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(eventData, results);
+        return results[0].gameObject;
+        Debug.Log("Get UI "+ results[0].gameObject.name);
+    }
+    
+}
--- Occult city builder/Assets/Scripts/UI/UIObject.cs
+++ Occult city builder/Assets/Scripts/UI/UIObject.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Mathematics;
@@ -6,45 +7,14 @@ using UnityEngine.EventSystems;
 
 public class UIObject : MonoBehaviour
 {
-    [SerializeField] private bool isCursorOverObject;
-    [SerializeField] private GameObject buildingToCreate;
-    private bool canIstantiateNewBuilding = true;
-    
    
-    void Update()
+    [SerializeField] private GameObject buildingToCreate;
+    
+
+    public void CreateBuldingFromUI()
     {
-        CheckIfMouseOverObject();
-        CheckIfCanTakeBuilding();
+        Instantiate(buildingToCreate, transform.position, quaternion.identity);
     }
 
-    private void CheckIfMouseOverObject()
-    {
-        if (GetMousePos())
-        {
-            isCursorOverObject = true;
-            Debug.Log("IAMUI");
-        }
-        else
-        {
-            isCursorOverObject = false;
-        }
-    }
-
-    bool GetMousePos()
-    {
-        return  EventSystem.current.IsPointerOverGameObject();
-    }
-
-    void CheckIfCanTakeBuilding()
-    {
-        if (isCursorOverObject && Input.GetMouseButtonDown(0))
-        {
-            Instantiate(buildingToCreate, transform.position, quaternion.identity);
-            canIstantiateNewBuilding = false;
-        }
-        else
-        {
-            canIstantiateNewBuilding = true;
-        }
-    }
+   
 }
