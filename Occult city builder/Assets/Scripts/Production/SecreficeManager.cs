289d6e55f70918dcb5eeaf71f43080b871360a64
ArtRadz <84411425+ArtRadz@users.noreply.github.com>
2022-06-16 01:38:44 +0300

    Timer and Sacrifice System
    Scripts for timer and sacrifice system. functions need to be connected to UI  buttons

--- Occult city builder/Assets/Scripts/Production/SecreficeManager.cs
+++ Occult city builder/Assets/Scripts/Production/SecreficeManager.cs
@@ -4,30 +4,40 @@ using UnityEngine;
 
 public class SecreficeManager : MonoBehaviour
 {
+    private bool isVilagers;
     [SerializeField] MonsterManager monster;
     [SerializeField] ResourceData resourceData;
     [SerializeField] private int amountOfReasourcesToSacrefice;
     [SerializeField] private float hungerReductionPerUnitOfResorce, powerIncreasePerUnitOfReasorce;
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [SerializeField] private float hungerReductionPerUnitOfCattle, powerIncreasePerUnitOfCattle;
+    [SerializeField] private float hungerReductionPerUnitOfVilagers, powerIncreasePerUnitOfVilagers;
     private void Sacrifice()
     {
         //to be called from UI Button
+
         monster.monsterHunger -= amountOfReasourcesToSacrefice * hungerReductionPerUnitOfResorce;
         monster.monsterPower += amountOfReasourcesToSacrefice * powerIncreasePerUnitOfReasorce;
+        if (isVilagers)
+        {
+            resourceData.vilagers -= amountOfReasourcesToSacrefice;
+        }
+        else
+        {
+            resourceData.cattle -= amountOfReasourcesToSacrefice;
+      }
+        amountOfReasourcesToSacrefice = 0;
+        isVilagers = false;
     }
-    private void ReasorcesToSacrifice()
+    private void ButtonForCattle()
     {
-        //will need a sacrifice UI,
-        //will decide based on button which resorce to take from ResourceData
-        //will decide based on resorce the value of hungerReductionPerUnitOfResorce && powerIncreasePerUnitOfReasorce
+        hungerReductionPerUnitOfResorce = hungerReductionPerUnitOfCattle;
+        powerIncreasePerUnitOfReasorce = powerIncreasePerUnitOfCattle;
+        isVilagers = false;
+    }
+    private void ButtonForVilagers()
+    {
+        hungerReductionPerUnitOfResorce = hungerReductionPerUnitOfVilagers;
+        powerIncreasePerUnitOfReasorce = powerIncreasePerUnitOfVilagers;
+        isVilagers = true;
     }
 }
