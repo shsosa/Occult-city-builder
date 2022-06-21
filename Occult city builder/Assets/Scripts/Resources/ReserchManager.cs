ad916b2645fc66161029879e3e76e31e14a3b3a2
shsosa <sh.sosa@gmail.com>
2022-06-21 19:03:08 +0300

    Lets do this shit

--- Occult city builder/Assets/Scripts/Resources/ReserchManager.cs
+++ Occult city builder/Assets/Scripts/Resources/ReserchManager.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class ReserchManager : MonoBehaviour
 {
+    
+    //todo tile cleaning 
+    //todo power reduction 
     // Start is called before the first frame update
     void Start()
     {
