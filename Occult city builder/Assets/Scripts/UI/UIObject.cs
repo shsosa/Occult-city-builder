57590c71e92c5db2ebaef56c9cf3241ec00245bd
sosa <sh.sosa@gmail.com>
2022-06-21 23:42:16 +0300

    Bigger UI

--- Occult city builder/Assets/Scripts/UI/UIObject.cs
+++ Occult city builder/Assets/Scripts/UI/UIObject.cs
@@ -1,3 +1,4 @@
+using System;
 using Game_managment;
 using Unity.Mathematics;
 using UnityEngine;
@@ -7,14 +8,23 @@ public class UIObject : MonoBehaviour
    //todo check if building manager can instantiate
     [SerializeField] private GameObject buildingToCreate;
     public ReasourcePrice _reasourcePrice;
+    private Transform map;
     public bool canBuild =false;
-    
-    
+
+    private void Awake()
+    {
+        map = FindObjectOfType<BuildingManager>().transform;
+    }
 
     public void CreateBuldingFromUI()
     {
         if(canBuild)
-            Instantiate(buildingToCreate, transform.position, quaternion.identity);
+        {
+            //todo instantiate from another place
+            GameObject building;
+            building = Instantiate(buildingToCreate, transform.position, quaternion.identity);
+            building.transform.SetParent(map);
+        }
     }
 
    
