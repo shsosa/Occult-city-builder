eb7456a76cc1c63acdb44f5dc818e0b8031f6499
ArtRadz <84411425+ArtRadz@users.noreply.github.com>
2022-07-04 08:31:03 +0300

    Music Manager

--- Occult city builder/Assets/Scripts/Music/MusicManager.cs
+++ Occult city builder/Assets/Scripts/Music/MusicManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MusicManager : MonoBehaviour
 {
-    [SerializeField] private float fadeRate;
+    [SerializeField] private float timeToFade;
     [SerializeField] AudioSource normalMusic;
     [SerializeField] AudioSource powerMusic;
     [SerializeField] AudioSource hungerMusic;
@@ -18,9 +18,12 @@ public class MusicManager : MonoBehaviour
     private void Start()
     {
         powerMusic.volume = 0;
+        powerMusic.Play();
         hungerMusic.volume = 0;
+        hungerMusic.Play();
         curentllyChosen =curentllyPlaying= normalMusic;
         curentllyPlaying.Play();
+
     }
     private void FixedUpdate()
     {
@@ -29,37 +32,42 @@ public class MusicManager : MonoBehaviour
 
     private void MusicSwitch()
     {
-        if (monster.monsterHunger >= hungerThreshhold)
-        {
-            curentllyChosen = hungerMusic;
-            
-        }
-        else if (monster.monsterPower >= powerThreshold)
-        {
-            
-            curentllyChosen = powerMusic;
-        }
-        else
-        {
-           
-            curentllyChosen = normalMusic;  
-        }
-        Music(curentllyPlaying, curentllyChosen);
-    }
-    private void Music(AudioSource audio1,AudioSource audio2)
-    {
-        if (curentllyChosen!=curentllyPlaying)
-        {
-            audio2.Play();
-            float currentTime = 0;
-            while (currentTime < fadeRate)
+        
+            if (monster.monsterHunger >= hungerThreshhold)
             {
-                currentTime ++;
-                audio1.volume = Mathf.Lerp(audio1.volume, 0, currentTime * fadeRate);
-                audio2.volume= Mathf.Lerp(audio2.volume, 1, currentTime * fadeRate);
-                
+                curentllyChosen = hungerMusic;
+
             }
-            curentllyPlaying = curentllyChosen;
+            else if (monster.monsterPower >= powerThreshold)
+            {
+
+                curentllyChosen = powerMusic;
+            }
+            else
+            {
+
+                curentllyChosen = normalMusic;
+            }
+          StartCoroutine(CrossFade(curentllyPlaying, curentllyChosen));
+        
+    }
+   
+    private IEnumerator CrossFade(AudioSource audio1, AudioSource audio2)
+    {
+        if (curentllyChosen.clip != curentllyPlaying.clip)
+        {
+           // audio2.Play();
+            float currentTime = 0;
+            while (currentTime < timeToFade)
+            {
+                audio1.volume = Mathf.Lerp(1, 0, currentTime / timeToFade);
+                audio2.volume = Mathf.Lerp(0, 1, currentTime / timeToFade);
+                currentTime += Time.deltaTime;
+                yield return null;
+            }
+            //audio1.Stop();
         }
+        curentllyPlaying = curentllyChosen;
+       // 
     }
 }
